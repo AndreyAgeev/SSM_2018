ac9f749a7f6adf43a19dac4bcad9caa98c520d32
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2018-11-23 16:26:09 +0300

    add result.h

---
+++ result.h
@@ -0,0 +1,39 @@
+#pragma once
+
+class Result
+{
+public:
+	vector<double> TMP;
+	vector<double> atsw1;
+	vector<double> wstorg;
+	vector<double> sowwat;
+	vector<double>  ISATSW;
+	vector<double>  ATSW;
+	vector<double> TTSW;
+	vector<double> FTSW;
+	vector<double> WSTORG;
+	vector<double>   ATSW1;
+	vector<double> TTSW1;
+	vector<double> WLL1;
+	vector<double>  WAT1;
+
+	vector<double> WSAT1;
+	vector<double> WLL;
+	vector<double>  WATRT;
+	vector<double>  WSAT;
+	double EOSMIN;
+	double WETWAT;
+	double KET;
+	double  DYSE;
+	double CTR;
+	double CE;
+	double CRUNOF;
+	double CIRGW;
+	double IRGNO;
+	double LAI;
+	double  LtDrCntr;
+	double SE2C;
+	double  SE1MX;
+	double  DSR;
+	double IRGW;
+};
