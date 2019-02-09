09c53f9ffe63c3756e6096757dcecf688bc450cf
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2019-02-09 17:06:14 +0300

    Update data.h
    Added code to read the length of the day from h5

--- data.h
+++ data.h
@@ -1,4 +1,5 @@
 #pragma once
+//#include <QStandardItemModel>
 #include <QtSql/qsqldatabase.h>
 #include <QtSql/qsqlquery.h>
 
@@ -22,6 +23,7 @@ public:
 			vector<double> tmax;
 			vector<double> tmin;
 			vector<double> rain;
+			vector<int> month;
 	};
 	struct Data_phen
 	{
@@ -98,7 +100,7 @@ public:
 		double  ttDKill;
 		double  LtFtsw;
 		double  LtWdDur;
-		double  vpd_resp;
+		int  vpd_resp;
 		double  vpd_cr;
 	};
 	Data_f data_h5;
@@ -106,6 +108,78 @@ public:
 	Data() {}
 	void read_ini(void)
 	{
+		/*QSettings sett("C:\project\SSM\SSM_improved\SSM_improved\crops.ini.src", QSettings::IniFormat);
+		sett.beginGroup("Jam");
+		data_p.phyl = sett.value("phyl", 46).toDouble();
+		data_p.PLACON = sett.value("PLACON", 1).toDouble();
+		data_p.PLAPOW30 = sett.value("PLAPOW30", 2.158).toDouble();
+		data_p.SLA = sett.value("SLA", 0.021).toDouble();
+		data_p.TBRUE = sett.value("TBRUE", 2).toDouble();
+		data_p.TP1RUE = sett.value("TP1RUE", 14).toDouble();
+		data_p.TP2RUE = sett.value("TP2RUE", 30).toDouble();
+		data_p.TCRUE = sett.value("TCRUE", 38).toDouble();
+		data_p.KPAR = sett.value("KPAR", 0.65).toDouble();
+		data_p.IRUE1 = sett.value("IRUE1", 1.8).toDouble();
+		data_p.IRUE2 = sett.value("IRUE2", 1.8).toDouble();
+		data_p.FLF1A = sett.value("FLF1A", 0.53).toDouble();
+		data_p.FLF1B = sett.value("FLF1B", 0.3).toDouble();
+		data_p.WTOPL = sett.value("WTOPL", 180).toDouble();
+		data_p.FLF2 = sett.value("FLF2", 0.13).toDouble();
+		data_p.FRTRL = sett.value("FRTRL", 0.22).toDouble();
+		data_p.GCF = sett.value("GCF", 1).toDouble();
+		data_p.PDHI = sett.value("PDHI", 0.02).toDouble();
+		data_p.WDHI1 = sett.value("WDHI1", 0).toDouble();
+		data_p.WDHI2 = sett.value("WDHI2", 0).toDouble();
+		data_p.WDHI3 = sett.value("WDHI3", 450).toDouble();
+		data_p.WDHI4 = sett.value("WDHI4", 2000).toDouble();
+		data_p.DEPORT = sett.value("DEPORT", 200).toDouble();
+		data_p.EED = sett.value("EED", 1000).toDouble();
+		data_p.GRTDP = sett.value("GRTDP", 17).toDouble();
+		data_p.TEC = sett.value("TEC", 5).toDouble();
+		data_p.WSSG = sett.value("WSSG", 0.3).toDouble();
+		data_p.WSSL = sett.value("WSSL", 0.4).toDouble();
+
+		data_p.WSSD = sett.value("WSSD", 0.4).toDouble();
+		data_p.SLNG = sett.value("SLNG", 2.3).toDouble();
+		data_p.SLNS = sett.value("SLNS", 0.78).toDouble();
+		data_p.SNCG = sett.value("SNCG", 0.025).toDouble();
+		data_p.SNCS = sett.value("SNCS", 0.0078).toDouble();
+		data_p.GNC = sett.value("GNC", 0.043).toDouble();
+		data_p.MXNUP = sett.value("MXNUP", 0.45).toDouble();
+		data_p.WSSN = sett.value("WSSN", 0.5).toDouble();
+		data_p.TBD = sett.value("TBD", 2).toDouble();
+		data_p.TP1D = sett.value("TP1D", 21).toDouble();
+		data_p.TP2D = sett.value("TP2D", 30).toDouble();
+
+		data_p.TCD = sett.value("TCD", 40).toDouble();
+		data_p.CPP = sett.value("CPP", 11).toDouble();
+		data_p.ppsen = sett.value("ppsen", 0.143).toDouble();
+		data_p.ttSWEM = sett.value("ttSWEM", 3.0).toDouble();
+		data_p.ttEMR1 = sett.value("ttEMR1", 30.3).toDouble();
+		data_p.ttR1R3 = sett.value("ttR1R3", 8.3).toDouble();
+		data_p.ttR3R5 = sett.value("ttR3R5 ", 7.3).toDouble();
+		data_p.ttR5R7 = sett.value("ttR5R7", 36.9).toDouble();
+		data_p.ttR7R8 = sett.value("ttR7R8", 6.0).toDouble();
+		data_p.ttBRP = sett.value("ttBRP", 3.0).toDouble();
+		data_p.ttTRP = sett.value("ttTRP", 33.3).toDouble();
+
+		data_p.ttWSD = sett.value("ttWSD", 3.0).toDouble();
+		data_p.ttR1TLM = sett.value("ttR1TLM", 8.3).toDouble();
+		data_p.ttR1TLP = sett.value("ttR1TLP", 15.6).toDouble();
+		data_p.ttRUE = sett.value("ttRUE", 30.3).toDouble();
+		data_p.ttBSG = sett.value("ttBSG", 48.9).toDouble();
+		data_p.ttTSG = sett.value("ttTSG", 85.8).toDouble();
+		data_p.ttBRG = sett.value("ttBRG", 3.0).toDouble();
+		data_p.ttTRG = sett.value("ttTRG", 48.9).toDouble();
+		data_p.ttBNF = sett.value("ttBNF", 14.7).toDouble();
+		data_p.TRESH = sett.value("TRESH", 0.75).toDouble();
+		data_p.ttDKill = sett.value("ttDKill", 48.9).toDouble();
+
+		data_p.LtFtsw = sett.value("LtFtsw", 0).toDouble();
+		data_p.LtWdDur = sett.value("LtWdDur ", 2).toDouble();
+		data_p.vpd_resp = sett.value("vpd_resp ", 1).toDouble();
+		data_p.vpd_cr = sett.value("vpd_cr", 20.0).toDouble();
+		sett.endGroup();*/
 		QSettings sett("C:\project\SSM\SSM_improved\SSM_improved\crops.ini.src", QSettings::IniFormat);
 		sett.beginGroup("Jam");
 		data_p.phyl = sett.value("phyl", 46).toDouble();
@@ -151,7 +225,7 @@ public:
 
 		data_p.TCD = sett.value("TCD", 40).toDouble();
 		data_p.CPP = sett.value("CPP", 18).toDouble();
-		data_p.ppsen = sett.value("ppsen", 0.1).toDouble();
+		data_p.ppsen = sett.value("ppsen", 0.11).toDouble();
 		data_p.ttSWEM = sett.value("ttSWEM", 5.0).toDouble();
 		data_p.ttEMR1 = sett.value("ttEMR1", 23.0).toDouble();
 		data_p.ttR1R3 = sett.value("ttR1R3", 9.0).toDouble();
@@ -179,14 +253,12 @@ public:
 		data_p.vpd_cr = sett.value("vpd_cr", 20.0).toDouble();
 		sett.endGroup();
 	}
-	void read_h5(QString file_name)
+	void read_h5(QString file_name, bool mode, vector<double>& dl)
 	{
 		try {
-			cout << "BEGIN READ" << endl;
             File file(file_name.toStdString(), File::ReadOnly);
 			DataSet doy_read = file.getDataSet("doy");
 			doy_read.read(data_h5.doy);
-			cout << "END READ" << endl;
 			DataSet rain_read = file.getDataSet("rain");
 			rain_read.read(data_h5.rain);
 			DataSet srad_read = file.getDataSet("srad");
@@ -197,10 +269,19 @@ public:
 			tmin_read.read(data_h5.tmin);
 			DataSet year_read = file.getDataSet("year");
 			year_read.read(data_h5.years);
+			if (mode == true)
+			{
+				DataSet dl_read = file.getDataSet("dl");
+				dl_read.read(dl);
+				DataSet month_read = file.getDataSet("month");
+				month_read.read(data_h5.month);
+			}
 		} 
 		catch (Exception &err){
 			std::cerr << err.what() << std::endl;
 		}
 		
 	}
+
+
 };
