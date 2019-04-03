9e3fa3e6cb87f1549c4f935bf0bc6f432a7b205c
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2019-04-03 11:11:39 +0300

    Update model.h
    Added error output

--- model.h
+++ model.h
@@ -1453,6 +1453,8 @@ public:
 		}
 		cout << training_error << endl;
 		cout << curr_error << endl;
+		cout << nl->get_l1_pen() << endl;
+		cout << nl->get_l2_pen() << endl;
 		if (param.print_trace > 0 && param.function_mode != SOLTANI_FUNC)
 		{
 			nl->print_trace(0);
